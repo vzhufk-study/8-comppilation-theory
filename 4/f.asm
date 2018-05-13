1752cd8d2cb3216ee9c739ed296cec8763e63112
Vadym Zhufiak <vzhufiak@bizico.com>
2018-05-13 17:19:33 +0300

    Added function into SPL Lab.

--- 4/f.asm
+++ 4/f.asm
@@ -7,11 +7,18 @@ Kilkisti globalnih zminnih: cgv=0
   3 :    1    1  |   OPR    1
   4 :    6    2  |   STI    2
   5 :    4    1  |   LDI    1
-  6 :    2  1000  |   LIT  1000
-  7 :    1    5  |   OPR    5
-  8 :    4    2  |   LDI    2
-  9 :    1    6  |   OPR    6
- 10 :    2   10  |   LIT   10
- 11 :    1    7  |   OPR    7
- 12 :    1    2  |   OPR    2
- 13 :    1   10  |   OPR   10
+  6 :    4    2  |   LDI    2
+  7 :    2    2  |   LIT    2
+  8 :    7   11  |   CAL   11
+  9 :    1    2  |   OPR    2
+ 10 :    1   10  |   OPR   10
+ 11 :    8    0  |   INI    0
+ 12 :    4   -4  |   LDI   -4
+ 13 :    2  1000  |   LIT  1000
+ 14 :    1    5  |   OPR    5
+ 15 :    4   -3  |   LDI   -3
+ 16 :    1    6  |   OPR    6
+ 17 :    2   10  |   LIT   10
+ 18 :    1    7  |   OPR    7
+ 19 :    1    9  |   OPR    9
+ 20 :    1   10  |   OPR   10
--- 4/f.fun
+++ 4/f.fun
@@ -1,1 +1,2 @@
+   task  1   (opysana)   2 (Kilkisti parametriv)  11 (adresa tociki vhodu)
    main  1   (opysana)   0 (Kilkisti parametriv)   0 (adresa tociki vhodu)
